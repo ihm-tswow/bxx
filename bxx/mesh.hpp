82f4a3993c7007ca4272ee325f46d8b91ff2027f
ihm-tswow <ihm-ts@protonmail.com>
2022-05-23 01:05:03 +0200

    move old mesh code and add some tests

--- bxx/mesh.hpp
+++ bxx/mesh.hpp
@@ -3,9 +3,92 @@
 #include "id.hpp"
 
 typedef struct Mesh bl_mesh;
+typedef struct MVert bl_vert;
+typedef struct MPoly bl_poly;
+typedef struct MLoop bl_loop;
+typedef struct MLoopUV bl_loop_uv;
+typedef struct MLoopCol bl_loop_col;
 
 namespace bxx
 {
+    class vert
+    {
+    public:
+        vert(bl_vert* vert);
+        void set_x(float value);
+        void set_y(float value);
+        void set_z(float value);
+        void set(float x, float y, float z);
+        float get_x();
+        float get_y();
+        float get_z();
+        bl_vert* get_raw();
+    private:
+        bl_vert* m_vert;
+    };
+
+    class loop
+    {
+    public:
+        loop(bl_loop* loop);
+        int get_edge();
+        int get_vert();
+        void set_edge(int edge);
+        void set_vert(int vert);
+        void set(int vert, int edge = 0);
+        bl_loop* get_raw();
+    private:
+        bl_loop* m_loop;
+    };
+
+    class poly
+    {
+    public:
+        poly(bl_poly* poly);
+        void set_loop_start(int start);
+        void set_loop_count(int count);
+        void set(int start, int count);
+        int get_loop_start();
+        int get_loop_count();
+        short get_material();
+        void set_material(short material);
+        bl_poly* get_raw();
+    private:
+        bl_poly* m_poly;
+    };
+
+    class uv_wrapper
+    {
+    public:
+        uv_wrapper(bl_loop_uv* uv);
+        void set_u(float u);
+        void set_v(float v);
+        void set(float u, float v);
+        float get_u();
+        float get_v();
+        bl_loop_uv* get_raw();
+    private:
+        bl_loop_uv* m_uv;
+    };
+
+    class color
+    {
+    public:
+        color(bl_loop_col* color);
+        void set_r(float r);
+        void set_g(float g);
+        void set_b(float b);
+        void set_a(float a);
+        void set(float r, float g, float b, float a = 255);
+        float get_r();
+        float get_g();
+        float get_b();
+        float get_a();
+        bl_loop_col* get_raw();
+    private:
+        bl_loop_col* m_col;
+    };
+
     class mesh : public id
     {
     public:
@@ -24,7 +107,14 @@ namespace bxx
         int get_num_polys();
         int get_num_loops();
 
+        bxx::vert get_vert(int index);
+        bxx::poly get_poly(int poly);
+        bxx::loop get_loop(int loop);
+        bxx::uv_wrapper get_uv(int loop);
+        bxx::color get_color(int loop);
+
         bl_mesh* get_raw();
+        static bxx::mesh create(std::string const& name);
     private:
         bl_mesh* m_raw;
     };
---
+++ tests/mesh_tests.cpp
@@ -0,0 +1,49 @@
+#include "tests.hpp"
+#include "mesh.hpp"
+#include "context.hpp"
+#include "exec.hpp"
+
+BXX_TEST(mesh_created_with_name) {
+    BXX_ASSERT_EQUAL(bxx::mesh::create("test_mesh").get_name(), "test_mesh");
+}
+
+BXX_TEST(mesh_starts_with_no_meshdata) {
+    bxx::mesh mesh = bxx::mesh::create("test_mesh");
+    BXX_ASSERT_EQUAL(mesh.get_num_verts(), 0);
+    BXX_ASSERT_EQUAL(mesh.get_num_polys(), 0);
+    BXX_ASSERT_EQUAL(mesh.get_num_loops(), 0);
+}
+
+static bxx::mesh create_basic_triangle()
+{
+    bxx::mesh mesh = bxx::mesh::create("test_mesh");
+    mesh.add_verts(3);
+    mesh.add_loops(3);
+    mesh.add_polygons(1);
+    mesh.get_loop(0).set(0);
+    mesh.get_loop(1).set(1);
+    mesh.get_loop(2).set(2);
+    mesh.get_poly(0).set(0, 3);
+    return mesh;
+}
+
+BXX_TEST(mesh_can_add_geometry) {
+    bxx::mesh triangle = create_basic_triangle();
+    BXX_ASSERT_EQUAL(triangle.get_num_verts(), 3);
+    BXX_ASSERT_EQUAL(triangle.get_num_loops(), 3);
+    BXX_ASSERT_EQUAL(triangle.get_num_polys(), 1);
+}
+
+BXX_TEST(mesh_can_add_channels) {
+    bxx::mesh tri = create_basic_triangle();
+    tri.add_uv_channel();
+    tri.add_color_channel();
+    tri.get_uv(0).set(0, 1);
+    tri.get_uv(1).set(2, 3);
+    tri.get_uv(2).set(4, 5);
+    tri.get_color(0).set(0, 0, 0, 0);
+    tri.get_color(1).set(0, 0, 0, 0);
+    tri.get_color(2).set(0, 0, 0, 0);
+}
+
+
