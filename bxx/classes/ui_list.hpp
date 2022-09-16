aba7feb20917afd8d479166424a761b8cfe80041
ihm-tswow <ihm-ts@protonmail.com>
2022-09-16 12:02:17 +0200

    merge ui_list item/active index parameters into templated structs
    - allows templating the ui_list itself for them

--- bxx/classes/ui_list.hpp
+++ bxx/classes/ui_list.hpp
@@ -37,13 +37,53 @@ namespace bxx
         GRID
     };
 
-    template <typename crtp>
+    template <typename property_type = python_object, typename owner_type = python_object>
+    class generic_property
+    {
+    public:
+        generic_property(owner_type owner, std::string const& name)
+            : m_owner(owner)
+            , m_id(name)
+        {}
+
+        property_type get_value() { return m_owner.getattr<property_type>(m_id); }
+        void set_value(property_type const& type) { return m_owner.getattr<property_type>(m_id); }
+
+        owner_type get_owner() { return m_owner; }
+        std::string get_id() { return m_id; }
+    private:
+        owner_type m_owner;
+        std::string m_id;
+    };
+
+    template <typename property_type = python_object, typename owner_type = python_object>
+    class generic_property_readonly
+    {
+    public:
+        generic_property_readonly(python_object owner, python_object prop)
+            : m_owner(owner_type(owner))
+            , m_property(property_type(prop))
+        {}
+
+        property_type& get_value() { return m_property; }
+        owner_type get_owner() { return m_owner; }
+    private:
+        owner_type m_owner;
+        property_type m_property;
+    };
+
+    template <
+        typename crtp,
+        typename item_type = python_object,
+        typename data_type = python_object,
+        typename active_type = python_object
+    >
     class ui_list : public property_class
     {
     public:
-        virtual void draw_item(python_object ctx, ui_layout layout, python_object data, python_object item, std::uint64_t icon, python_object active_data, std::string const& active_propname) = 0;
+        virtual void draw_item(python_object ctx, ui_layout layout, generic_property_readonly<item_type,data_type> item, generic_property<std::int64_t,active_type> active_index, std::uint64_t icon) = 0;
         virtual void draw_filter(python_object ctx, ui_layout layout) {}
-        virtual python_tuple filter_items(python_object ctx, python_object data, python_object propname) { return python_tuple(); }
+        virtual python_tuple filter_items(python_object ctx, generic_property<item_type,data_type> item) { return python_tuple(); }
 
         PYFIELD_STRINGENUM(ui_list_layout_type, layout_type)
         PYFIELD(std::string, filter_name)
@@ -66,7 +106,13 @@ namespace bxx
             builder.read_properties_from(tmp_builder);
 
             builder.set_draw_item([this](python_object self, python_object ctx, python_object layout, python_object data, python_object item, python_object icon, python_object active_data, python_object active_propname) {
-                crtp(self.get_pyobject()).draw_item(ctx, layout.get_pyobject(), data, item, details::py2cxx<std::uint64_t>(icon), active_data, details::py2cxx<std::string>(active_propname));
+                crtp(self.get_pyobject()).draw_item(
+                    ctx,
+                    layout.get_pyobject(),
+                    generic_property_readonly<item_type,data_type>(data, item),
+                    generic_property<std::int64_t,active_type>(active_data, details::py2cxx<std::string>(active_propname)),
+                    details::py2cxx<std::uint64_t>(icon)
+                );
             });
 
             builder.set_draw_filter([this](python_object self, python_object ctx, python_object layout) {
@@ -75,8 +121,8 @@ namespace bxx
 
             if (!std::is_same<decltype(& crtp::filter_items), decltype(&ui_list::filter_items)>::value)
             {
-                builder.set_filter_items([this](python_object self, python_object ctx, python_object layout, python_object prop) {
-                    return crtp(self.get_pyobject()).filter_items(ctx, layout, prop);
+                builder.set_filter_items([this](python_object self, python_object ctx, python_object data, python_object propname) {
+                    return crtp(self.get_pyobject()).filter_items(ctx, generic_property<item_type,data_type>(data, details::py2cxx<std::string>(propname)));
                 });
             }
         }
